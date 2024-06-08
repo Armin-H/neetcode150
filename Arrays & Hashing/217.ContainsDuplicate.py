ea38239d6f1d95c499da83c2525f03f6dc2df7dc
Armin-H <armin.hejazian@yahoo.com>
2024-06-09 01:17:37 +1000

    added a second solution

--- Arrays & Hashing/217.ContainsDuplicate.py
+++ Arrays & Hashing/217.ContainsDuplicate.py
@@ -1,3 +1,17 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
+        #Time complexity : O(n), Space complexity O(n)
         return True if len(set(nums)) != len(nums) else False
+
+    def containsDuplicate(self, nums: List[int]) -> bool: 
+        #neetcode's solution(i was able to come up with this exact solution)
+        #Time complexity : O(n), Space complexity: O(n)
+        #This solution has a better best-cast time and space complexity than the previous solution
+        hashset = set()
+        
+        for n in nums: 
+            if n in hashset: 
+                return True
+            else: 
+                hashset.add(n)
+        return False
