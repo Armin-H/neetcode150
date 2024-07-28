5ec37dfa8e7f419729b601fe0ab37e5c10995efb
Armin-H <armin.hejazian@yahoo.com>
2024-07-29 07:22:29 +1000

    added neetcode's solution

--- LinkedList/19.RemoveNthNodefromEndofList.py
+++ LinkedList/19.RemoveNthNodefromEndofList.py
@@ -28,3 +28,19 @@ class Solution:
 
         return head
             
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        """neetcode's solution using two pointer approach"""
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+        return dummy.next
